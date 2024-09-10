9d0a79365336d8a07b9dde9da9b4a9dc96230165
Agape Mugisha <mugishaagape1@gmail.com>
2024-09-10 12:23:16 +0800

    The name of file is updated

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 10
+number2 = 5
+total = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print ("Addition of", number1, "and", number2, "is", total)
+print ("Substraction of" number1 "and" number2 "is" difference)
+print ("Multiplication of", number1, "and", number2, "is", product)
+
+
+
+
+ 
