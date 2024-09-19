1f5c2919cdba994176daabb1016d3b8a46638088
Agape Mugisha <mugishaagape1@gmail.com>
2024-09-19 12:05:06 +0800

    updates

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,19 +1,19 @@
-number1 = int(input("enter the first number"))
-number2 = int(input("enter the second number"))
+number1 = int(input("Enter the first number"))
+number2 = int(input("Enter the second number"))
 operator = str(input("Choose the operation (+, -, *, /):"))
 
 match operator :
     case "+" :
-        print(f"the result is {number1 + number2}")
+        print(f"The result is {number1 + number2}")
     case "-" :
-        print(f"the result is {number1 - number2}")
+        print(f"The result is {number1 - number2}")
     case "*" :
-        print(f"the result is {number1 * number2}")
+        print(f"The result is {number1 * number2}")
     case "/" :
         if number2 == 0 :
             print("Cannot divide by zero")
         else :
-            print(f"the result is {number1 / number2}")
+            print(f"The result is {number1 / number2}")
     case _ :
-        print("the operator is invalid")
+        print("The operator is invalid")
         
