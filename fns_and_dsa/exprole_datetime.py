8feb2827a0ff1b787b8ba0dc1c029e82392c603e
Agape Mugisha <mugishaagape1@gmail.com>
2024-09-28 16:03:53 +0800

    add the the exprol file

---
+++ fns_and_dsa/exprole_datetime.py
@@ -0,0 +1,12 @@
+import datetime
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print(f"current date {current_date}")
+display_current_datetime()
+number_of_days = int(input("Enter numbe of days: "))
+def calculate_future_date(number_of_days):
+    days = datetime.timedelta(days=number_of_days)
+    future_date = datetime.datetime.now() + days
+    print(f"Future date: {future_date}")
+calculate_future_date(number_of_days)
+    
