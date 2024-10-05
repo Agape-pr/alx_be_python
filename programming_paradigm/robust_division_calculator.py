8d70d96ca2d6f6f259f549bedfda3e082eab235f
Agape Mugisha <mugishaagape1@gmail.com>
2024-10-05 14:46:34 +0800

    updated

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,7 +7,7 @@ def safe_divide(numerator, denominator):
         print("Error: Cannot divide by zero.")
         return False
     except ValueError :
-        print("Erroe: Please enter numeric values only.")
+        print("Error: Please enter numeric values only.")
         return False
     else:
         return result
