1ea95beced11899d8557e4eebc67a8bde0f4a904
Agape-pr <mugishaagapelearning@gmail.com>
2024-10-10 11:54:03 +0000

    added library managemnt file that interacts with the main.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,18 +1,21 @@
-class book:
+class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = false
-class library :
-    def __init__(self, book):
+        self._is_checked_out = False
+class Library :
+    def __init__(self, Book):
         self._books = []
-    def add_book(self):
-        self._books.append(book)
-    def check_out_book(self, title):
+    def add_book(self, Book):
+        self._books.append(Book)
+    def check_out_book(self, Book):
         for book in self._books:
-            if book.title == title:
-                self._books.remove(book)
+            if book.title == Book:
+                self._books.remove(Book)
                 break
-        for book in self._books :
-            print(book)
 
+    def return_book(self, Book):
+        self._books.append(Book)
+    def list_available_books(self):
+        for book in self._books:
+            print(book)
