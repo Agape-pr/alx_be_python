f95b8ad164ad6bf56afdc4f9523c5d2a7e06b16f
Agape-pr <mugishaagapelearning@gmail.com>
2024-10-11 13:38:51 +0000

    added

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,18 +2,23 @@ class Book:
     def __init__(self, title : str, author : str):
         self.title = title
         self.author = author
+    def __str__(self):
+        return f"book {self.title} by {self.author} "
 
 
 class EBook(Book):
     def __init__(self, title : str, author : str, file_size : int):
         super().__init__(title, author)
         self.file_size = file_size
-        
+    def __str__(self):
+        return f"book {self.title} by {self.author} "
 
 class PrintBook(Book):
     def __init__(self, title : str, author : str, page_count : int):
         super().__init__(title, author)
         self.page_count = page_count
+    def __str__(self):
+        return f"book {self.title} by {self.author} "
 
 class Library :
     def __init__(self) :
