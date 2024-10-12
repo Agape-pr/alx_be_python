d9c6244aef46b9422c4a80f7299af47c75bd29eb
Agape-pr <mugishaagapelearning@gmail.com>
2024-10-12 14:19:54 +0000

    the file is updated

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -18,7 +18,7 @@ class Rectangle(Shape):
     def area(self):
        return self.length * self.width
 
-class circle(Shape):
+class Circle(Shape):
      def __init__(self, radius):
           self.radius = radius
           super().__init__()
